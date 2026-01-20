101f36be3ec2ce4839ee7abb495be9cd429a6d89
Mahmud <wolfblue067@gmail.com>
2026-01-20 09:43:11 -0800

    Add basic calculator functionality in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,18 @@
+#project calculator
 
+
+x=float(input("enter any number : "))
+operator=input("enter an operator (+,-,*,/) : ")
+y=float(input("enter any  number :"))
+if operator == "+":
+    print(f"the addition is :{x + y} ")
+elif operator=="-":
+    print(f"the subtraction is :{x - y}")
+elif operator=="*":
+    print(f"the multiplication is :{x *y}")
+elif operator=="/":
+    print(f"the fraction is : {x/y}")
+elif operator =="**":
+    print(f"{x ** y}")
+elif operator=="%":
+    print(f"{x%y}")
